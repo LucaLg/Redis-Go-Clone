6580a61d238c7bb6af9a9ec713c926412acdb512
Luca <lucal.gno@gmail.com>
2024-04-11 10:14:54 +0200

    stage 5

--- app/parser.go
+++ app/parser.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -13,18 +14,34 @@ func parse(input []byte) (string, error) {
 		arrayLength += string(input[i])
 		i++
 	}
+	inputArr := strings.Split(string(input), "\r\n")
+	// *2\r\n$4\r\necho\r\n$3\r\nhey\r\n
+	// [0] *2
+	// [1] $4
+	// [2] echo
+	// [3] $3
+	// [4] hey
 	num, err := strconv.Atoi(arrayLength)
-	fmt.Println("Array length: ", num)
-	if num == 1 {
-		return "+PONG\r\n", nil
+	cmdArr := make([]string, num)
+	for _, inputString := range inputArr {
+		if inputString[0] != '$' && inputString[0] != '*' {
+			cmdArr = append(cmdArr, inputString)
+		}
 	}
 	if err != nil {
 		fmt.Println("Error parsing array length: ", err.Error())
 		return "", err
 	}
 
-	return string(""), nil
+	return handleCmds(cmdArr)
 }
-func parseWord(word []byte) string {
-	return string(word)
+func handleCmds(cmdArr []string) (string, error) {
+	switch cmdArr[0] {
+	case "echo":
+		return cmdArr[1], nil
+	case "ping":
+		return "+PONG\r\n", nil
+	default:
+		return "", fmt.Errorf("Unknown command: %s", cmdArr[0])
+	}
 }
