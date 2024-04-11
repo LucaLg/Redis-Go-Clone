4cc7110b50bf6a5cc20657dc53a5ca8902da71f0
Luca <lucal.gno@gmail.com>
2024-04-11 11:03:57 +0200

    stage 5

--- app/server.go
+++ app/server.go
@@ -12,17 +12,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	sem := make(chan struct{}, 100)
+	// sem := make(chan struct{}, 100)
 	for {
 		con, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		sem <- struct{}{}
 		go func(con net.Conn) {
 			handleClient(con)
-			<-sem
 		}(con)
 	}
 }
