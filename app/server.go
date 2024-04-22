708d19d1b3934a9bbd04b0b5377718a2b13750a9
Luca <lucal.gno@gmail.com>
2024-04-22 19:39:44 +0200

    Replication stage 10

--- app/server.go
+++ app/server.go
@@ -183,5 +183,5 @@ func (s *Server) handleCmds(cmdArr []string, conn net.Conn) (string, error) {
 }
 func (s *Server) handleRDBFile() string {
 	emptyFileHex := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-	return fmt.Sprintf("%d\r\n%s", len(emptyFileHex), emptyFileHex)
+	return fmt.Sprintf("$%d\r\n%s", len(emptyFileHex), emptyFileHex)
 }
