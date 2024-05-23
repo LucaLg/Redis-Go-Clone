93bb9a49a4cab74efc695b6ce73f9332e3d96d55
Luca <lucal.gno@gmail.com>
2024-05-23 20:56:54 +0200

    codecrafters submit [skip ci]

--- app/store.go
+++ app/store.go
@@ -76,46 +76,89 @@ func (s *Store) storeStream(id string, key string, pairs []KeyValPair) string {
 	entries, exists := s.Stream[key]
 	entry := Entry{id: id, pairs: pairs}
 	if !exists || len(entries) == 0 || id == "0-0" {
-		validID, err := checkID(id, "0-0")
+		validID, validatedID, err := checkID(id, "0-0")
+		entry.id = validatedID
 		errmsg := "ERR The ID specified in XADD must be greater than 0-0"
 		if !validID || err != nil {
 			return fmt.Sprintf("-%s\r\n", errmsg)
 		}
 		s.Stream[key] = []Entry{entry}
 	} else {
-		lastID := entries[len(entries)-1].id
-		validID, err := checkID(id, lastID)
+		// lastID := entries[len(entries)-1].id
+		lastID := findLastID(id, entries)
+		fmt.Println("Last ID Found: ", lastID)
+		validID, validatedID, err := checkID(id, lastID)
 		errmsg := "ERR The ID specified in XADD is equal or smaller than the target stream top item"
+		fmt.Println("Valid id ", validatedID)
 		if !validID || err != nil {
 			return fmt.Sprintf("-%s\r\n", errmsg)
 		}
+		entry.id = validatedID
 		s.Stream[key] = append(entries, entry)
 	}
-	return fmt.Sprintf("+%s\r\n", id)
+	return fmt.Sprintf("+%s\r\n", entry.id)
 }
-func checkID(id string, lastId string) (bool, error) {
+func findLastID(id string, entries []Entry) string {
+	time := strings.Split(id, "-")[0]
+	for i := len(entries) - 1; i > 0; i-- {
+		t := strings.Split(entries[i].id, "-")[0]
+		if t == time {
+			return entries[i].id
+		}
+	}
+	return entries[len(entries)-1].id
+}
+func checkID(id string, lastId string) (bool, string, error) {
+	if strings.Contains(id, "*") {
+		newID, err := genID(id, lastId)
+		if err != nil {
+			return false, id, err
+		}
+		id = newID
+	}
 	msID, seqID, err := getNumValueOfID(id)
 	if err != nil {
-		return false, err
+		return false, id, err
 	}
 	msLastID, seqLastID, err := getNumValueOfID(lastId)
 	if err != nil {
-		return false, err
+		return false, id, err
 	}
 	// lastID is 0-0 when there is no entry eg. the given id is the firs id
 	if lastId == "0-0" {
-		return msID > 0 || seqID > 0, nil
+		return msID > 0 || seqID > 0, id, nil
 	} else {
 		if msID < msLastID {
-			return false, nil
+			return false, id, nil
 		} else {
 			if msID == msLastID {
-				return seqID > seqLastID, nil
+				return seqID > seqLastID, id, nil
 			} else {
-				return true, nil
+				return true, id, nil
 			}
 		}
 	}
+}
+func genID(id string, lastID string) (string, error) {
+	parts := strings.Split(id, "-")
+	partsLast := strings.Split(lastID, "-")
+	if parts[1] == "*" {
+		lastSeq, err := strconv.Atoi(partsLast[1])
+		if err != nil {
+			return "", fmt.Errorf("coulndt parse last sequence number")
+		}
+		if lastSeq == 0 && partsLast[0] == "0" {
+			parts[1] = "1"
+		} else {
+			if partsLast[0] == parts[0] {
+				newSeq := lastSeq + 1
+				parts[1] = fmt.Sprintf("%d", newSeq)
+			} else {
+				parts[1] = "0"
+			}
+		}
+	}
+	return fmt.Sprintf("%s-%s", parts[0], parts[1]), nil
 }
 func getNumValueOfID(id string) (int, int, error) {
 	nums := strings.Split(id, "-")
@@ -124,11 +167,11 @@ func getNumValueOfID(id string) (int, int, error) {
 	}
 	ms, err := strconv.Atoi(nums[0])
 	if err != nil {
-		return -1, -1, fmt.Errorf("seconds couldnt be parsed  ")
+		return -1, -1, fmt.Errorf("ms couldnt be parsed  ")
 	}
 	seq, err := strconv.Atoi(nums[1])
 	if err != nil {
-		return -1, -1, fmt.Errorf("ms couldnt be parsed  ")
+		return -1, -1, fmt.Errorf("sequence num couldnt be parsed  ")
 	}
 	return ms, seq, nil
 }
--- app/store_test.go
+++ app/store_test.go
@@ -3,4 +3,6 @@ package main
 import "testing"
 
 func TestStore(t *testing.T) {
+	t.Run("find last entrie with right time ", func(t *testing.T) {
+	})
 }
